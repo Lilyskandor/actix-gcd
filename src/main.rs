a2119024be09dc763c462e1f83732e40a6a5f40b
Alexandre CAILLIEZ <cailliez.alexandre@hotmail.fr>
2022-12-13 18:34:57 +0200

    Server skeleton and From v0.1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
+#[allow(dead_code, unused_imports)]
 use actix_web::{web, App, HttpResponse, HttpServer};
 
 fn main() {
-    println!("Hello, world!");
+    // '||' is a closure : A value called as if it was a function. From a pool of requests, retrieves the App value that tells how to route and handle requests
+    let server = HttpServer::new( || {
+        App::new()
+            .route("/", web::get().to(get_index))
+    });
+
+    println!("Serving on http://localhost:3000...");
+    server
+        .bind("127.0.0.1:3000").expect("Error binding server to address")
+        .run().expect("Error running server");
+}
+
+fn get_index() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post"/>
+                    <input type="text" name="First number"/>
+                    <input type="text" name="Second number"/>
+                    <button type="submit">Compute GCD</button>
+                </form>
+            "#,
+        )
 }
